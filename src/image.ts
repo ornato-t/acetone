7ef2de61dffd03291c241ee63dfc5bf1bdb84bda
Tommaso Ornato <tommy.ornato@gmail.com>
2023-11-26 00:56:27 +0100

    Adding image compare logic

--- src/image.ts
+++ src/image.ts
@@ -1,13 +1,30 @@
-import Jimp from "jimp";
+import jimp from "jimp";
+import sharp from 'sharp';
 
 const SOURCE_URL = 'https://cdn.discordapp.com/emojis/1178086838561407148.webp?size=96';
-const SOURCE_IMAGE =await Jimp.read(SOURCE_URL);
+const SOURCE_IMAGE = await read(SOURCE_URL);
 
 export async function compareImage(imageUrl: string) {
     const jimage1 = SOURCE_IMAGE;
-    const jimage2 = await Jimp.read(imageUrl);
+    const jimage2 = await read(imageUrl);
 
-    const diff = Jimp.diff(jimage1, jimage2, .1);
+    const diff = jimp.diff(jimage1, jimage2, .1);
 
     return diff.percent;
+}
+
+/*
+    Processes an image and brings it in the Jimp format
+    1. Fetches the image
+    2. Extracts it
+    3. Converts it to PNG through Sharp (so Jimp can digest it)
+    4. Converts it to a Jimp image
+    5. Passes it through a grayscale 
+*/
+async function read(url: string) {
+    const res = await fetch(url);
+    const bufferWebp = await res.arrayBuffer();
+    const bufferPng = await sharp(bufferWebp).toFormat('png').toBuffer();
+    const jimpImage = await jimp.read(bufferPng);
+    return jimpImage.greyscale();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
 import { compareImage } from './image';
 
-const res = await compareImage('https://cdn.discordapp.com/emojis/1178086838561407148.webp?size=96');
-console.log(res);
+const list = await getEmojiList();
+for (const emoji of list) {
+    console.log(await isTarget(emoji.path, 0.15), '\t', emoji.name);
+}
+
+async function getEmojiList() {
+    const res = await fetch("https://discord.com/api/guilds/SERVER_ID/emojis?limit=1000", {
+        headers: {
+            Authorization: "Bot TOKEN",
+        }
+    });
+
+    const json = await res.json() as { id: string, name: string }[];
+
+    return json.map(emoji => ({ name: emoji.name, path: emoji.id }))
+}
+
+
+async function isTarget(id: string, tolerance: number) {
+    const url = `https://cdn.discordapp.com/emojis/${id}.webp?size=96`;
+    const match = await compareImage(url);
+
+    return match <= tolerance;
+}
