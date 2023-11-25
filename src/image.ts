201c4a3400a7ddf340f5ccaad76eec4845f68b66
Tommaso Ornato <tommy.ornato@gmail.com>
2023-11-25 23:50:07 +0100

    New image detection format

--- src/image.ts
+++ src/image.ts
@@ -1,30 +1,13 @@
-import pixelmatch from 'pixelmatch';
-import sharp from 'sharp';
+import Jimp from "jimp";
 
-const SOURCE_URL = 'https://cdn.discordapp.com/emojis/1178086838561407148.webp';
-const SOURCE_IMAGE = await readImage(SOURCE_URL);
-
-
-async function readImage(url: string) {
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
-
-    const image = sharp(Buffer.from(arrayBuffer));
-    const res = await image.greyscale().raw().toBuffer();
-
-    return new Uint8Array(res);
-}
+const SOURCE_URL = 'https://cdn.discordapp.com/emojis/1178086838561407148.webp?size=96';
+const SOURCE_IMAGE =await Jimp.read(SOURCE_URL);
 
 export async function compareImage(imageUrl: string) {
-    const img1 = SOURCE_IMAGE;
-    const img2 = await readImage(imageUrl);
-    const diff = new Uint8Array(96 * 96 * 4); // RGBA
+    const jimage1 = SOURCE_IMAGE;
+    const jimage2 = await Jimp.read(imageUrl);
 
-    const numDiffPixels = pixelmatch(
-        img1, img2, diff,
-        96, 96,
-        { threshold: 0.1 }
-    );
+    const diff = Jimp.diff(jimage1, jimage2, .1);
 
-    return numDiffPixels;
+    return diff.percent;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { compareImage } from './image';
 
-const res = await compareImage('');
+const res = await compareImage('https://cdn.discordapp.com/emojis/1178086838561407148.webp?size=96');
 console.log(res);
