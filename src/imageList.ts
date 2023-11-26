b58183fb110bd9d22d3c5146ba25a8c4f53902ac
Tommaso Ornato <tommy.ornato@gmail.com>
2023-11-26 23:20:45 +0100

    Adding emoji to forbidden list

--- src/imageList.ts
+++ src/imageList.ts
@@ -1,17 +1,26 @@
 const emojiList = [
     {
         name: 'Nail care',
-        path: 'https://cdn.discordapp.com/emojis/1178086838561407148.webp?size=96'
+        path: getUrl('1178086838561407148'),
     },
     {
         name: 'Reversed nail care',
-        path: 'https://cdn.discordapp.com/emojis/1178439074726559795.webp?size=96'
+        path: getUrl('1178439074726559795'),
+
+    },
+    {
+        name: 'Blood nail care',
+        path: getUrl('1178447641642811502'),
     },
 ] satisfies Emoji[];
 
 interface Emoji {
     name: string,
     path: string,
+}
+
+function getUrl(id: string) {
+    return `https://cdn.discordapp.com/emojis/${id}.webp?size=96`;
 }
 
 export default emojiList;
