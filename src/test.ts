040b3b6aabce6d826df0a7ff02aa513bf56e3830
Tommaso Ornato <tommy.ornato@gmail.com>
2023-11-27 10:58:13 +0100

    Improving test script, adding tests for whole emoji library

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,30 @@
+import 'dotenv/config';
 import { isTarget } from "./image.js";
 import { TOLERANCE } from "./index.js";
 
-const id = '1178489718447550494';
+await test('931407728000237588');
+await testEmoji();
 
-const res = await isTarget(id, TOLERANCE);
-console.log('Test:', res.result, res.bestMatch.name, res.bestMatch.match);
 process.exit(0);
+
+//Test an emoji, by its ID against the Acetone Detection Algorithm, prints the result
+async function test(id: string) {
+    const res = await isTarget(id, TOLERANCE);
+
+    console.log(`Test: ${res.result}, closest match found: ${res.bestMatch.name} with a ${(res.bestMatch.match * 100).toFixed(2)}% match`);
+}
+
+//Test the entire emoji library against the Acetone Detection Algorithm, prints any positive matches
+async function testEmoji() {
+    const res = await fetch('https://discord.com/api/guilds/826405737093136434/emojis', {
+        headers: new Headers({ Authorization: `Bot ${process.env.DISCORD_TOKEN}` })
+    });
+    const emojiList = await res.json() as { id: string, name: string }[];
+
+    for(const emoji of emojiList) {
+        const testResult = await isTarget(emoji.id, TOLERANCE);
+        if(true || testResult.result) {
+            console.warn(`Emoji ${emoji.name} matches: ${testResult.result}, closest match found: ${testResult.bestMatch.name} with a ${(testResult.bestMatch.match * 100).toFixed(2)}% match`);
+        }
+    }
+}
